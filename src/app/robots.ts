b624455b0a9faddaa3f6e27cc630b116103f08d2
coderamrin <coderamrin@gmail.com>
2024-07-17 16:05:14 +0600

    updated robots file

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,11 +4,13 @@ const URL = process.env.SITE_URL;
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: "/private/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/private/", "/images/icons/favicon.ico"],
+      },
+    ],
     sitemap: `${URL}/sitemap.xml`,
   };
 }
